08ad3ce95572215fecc32bd33ad37465d1093839
matiasArr <matias.arriagada1801@alumnos.ubiobio.cl>
2021-12-14 10:53:08 -0300

    a

--- main.c
+++ main.c
@@ -1,4 +1,5 @@
-//#include <nSystem.h>
+#include <nSystem.h>
+
 typedef struct{
     nMonitor nom;
     int size;
@@ -6,10 +7,34 @@ typedef struct{
     
 }* buff;
 
-int ofrecer (double precio){
-    nWait()
 
-    /* FUNCIONO
+nMonitor mon;
+PriQueue *MakePriQueue();
+
+nCondition adjudicacion;//son los procesos que son candidatos a adjudicarce una unidad 
+nCondition ofertas;
+int punidades;
+/*s
+    nCondition nMakeCondition(nMonitor mon): es como un cola de espera, donde quedan los procesos esperando
+    void nWaitCondition (nCondition cond): el proceso se queda en espera pasiva
+    void nSignalCondition(nCondition cond): despierta los procesos(grupo de procesos) 
+
+
+*/
+int ofrecer (double precio){
+   //Se ingresa el monitor solo cuando quiero protejer algo
+   // se tiene que guardar los precios
+   //Patron de uso
+   
+    nEnter(mon);
+    while(//no se llame a adjudicar)
+        nWaitCondition(adjudicacion);
+    nNotifyAll(m);
+
+    nExit(mon);    
+
+   
+    /* 
         Esta funcion espera hasta que se llame a la funcion adjudicar en cuyo caso se retornA TRUE
         y el hilos que llama es el ganador con otros que ofrecioron un precio mayor
         Si el precio es menor al que ofrecioron los otros retorna FALSE
@@ -18,6 +43,12 @@ int ofrecer (double precio){
 }
 
 double adjudicar(int *punidades){
+    nEnter(mon);
+        while(){
+
+        }
+        nSignalCondition(adjudicacion);
+    nExit(mon);
     /*
         Determina el ganador haciendo que todas las llamada a ofrecer retorne TRUE 
         Retorna el monto recaudado = suma(precios ofrecidos)
